cfb925a5eb410e5b0a32e0f2942b4d7f2b846df9
Charan <charanmyneni111@gmail.com>
2025-10-17 01:38:02 +0530

    PollBack Completed

--- backend/src/libs/judge0.libs.js
+++ backend/src/libs/judge0.libs.js
@@ -10,6 +10,31 @@ export const getJudge0LanguageId = (language) => {
     return languageMap[language.toUpperCase()] || null;
 };
 
+
+export const pollBatchResults = async (tokens) => {
+    while (true) {
+        const {data} = await axios.get(`${process.env.JUDGE0_API_URL}/submissions/batch`, {
+            params:{
+                tokens: tokens.join(','),
+                base64_encoded: false,
+
+            }
+        });
+
+        const results = data.submissions;
+
+        const isAllDone = results.every((r) => result.status.id >= 3);
+
+        if (isAllDone) {
+            return results;
+        }
+
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    }
+} 
+
+
 export const submitBatch = async (submissions) => {
 
     const {data} = await axios.post(`${process.env.JUDGE0_API_URL}/submissions/batch?base64_encoded=false`, {
