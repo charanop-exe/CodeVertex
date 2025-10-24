b6a1526f30c4930bd06d17c1a729a4de05f18cd9
Charan <charanmyneni111@gmail.com>
2025-10-24 18:13:37 +0530

    signup and login added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,33 @@
 import React from "react";
-
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
 
 const App = () => {
+  let authUser = 0
+
   return (
     <>
-      <h1 className="text-3xl bold">Hello World</h1>
+      {/* <h1 className="flex items-center justify-center">Hello World</h1> */}
+      <div className="flex justify-center items-center flex-col text-3xl font-extrabold">
+        <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to = {"/login"}/>}
+        />
+
+        <Route 
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to={"/"}/>}
+        />
+
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to = {"/"}/>}
+        />
+      </Routes>
+      </div>
     </>
   )
 
